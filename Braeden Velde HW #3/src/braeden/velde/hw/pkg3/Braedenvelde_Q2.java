fa83bbab21b9188ee7f735a5a32561b5380a171d
norm1000100 <norm1000100@gmail.com>
2017-11-01 15:08:02 -0700

    q1 complete onto q2
    Q1 completed and I have begun on Q2

--- Braeden Velde HW #3/src/braeden/velde/hw/pkg3/Braedenvelde_Q2.java
+++ Braeden Velde HW #3/src/braeden/velde/hw/pkg3/Braedenvelde_Q2.java
@@ -3,20 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package braeden.velde.hw.pkg3;
+
+import java.util.Scanner;
 
 /**
  *
- * @author velde
+ * @author Cool__000
  */
-import java.util.Scanner;
-public class BraedenVeldeHW3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public class Braedenvelde_Q2 {
+        public static void main(String[] args) {
         // TODO code application logic here
     
     int residence , hours;
@@ -34,7 +30,20 @@ public class BraedenVeldeHW3 {
     System.out.printf("%s%n%s%n%s%n%s%n","Would you like to know which pet you should get?",
             "Then tell me what kind of residence you live in!","Choose 1, 2, or 3 from:","(1)House, (2)Apartment, or (3)Dormitory");
     residence = kbd.nextInt();
-    if (residence == 1 || residence == 2 || residence == 3)
+    switch (residence)
+    {
+        case 1:
+        case 2:
+        case 3: {
+            System.out.printf("%s%n%s%n%s%n", "Now tell me, How many hours are you home during the day?"
+        , "Choose 1, 2, 3, 4, or 5 from:" , "(1)18 or more, (2)10-17, (3)8-9, (4)6-7, (5)0-5");
+      hours = kbd.nextInt();
+      
+      switch (hours)
+      {
+          case 1:
+      }
+            
     {
         System.out.printf("%s%n%s%n%s%n", "Now tell me, How many hours are you home during the day?"
         , "Choose 1, 2, 3, 4, or 5 from:" , "(1)18 or more, (2)10-17, (3)8-9, (4)6-7, (5)0-5");
@@ -114,3 +123,5 @@ public class BraedenVeldeHW3 {
     
 }
 }
+
+
